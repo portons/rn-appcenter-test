41634941aa6bd3e3b05f6811fea801c2e620fa4d
Vova Porton <vova@500tech.com>
2018-12-03 09:20:58 +0200

    Various configurations

--- App.js
+++ App.js
@@ -7,7 +7,8 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import Crashes from 'appcenter-crashes';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -18,6 +19,10 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+  componentDidMount() {
+
+  }
+
   render() {
     return (
       <View style={styles.container}>
